3b6b0f3805ec09f8935e2586d43f110984ad70eb
wanghaha1256 <1256136896@qq.com>
2021-12-01 14:51:43 +0800

    ignore

--- configs/code.go
+++
@@ -1,9 +0,0 @@
-package configs
-
-const (
-	SUCCESS      = 200
-	ERROR        = 500
-	BAD_REQUEST  = 400
-	UNAUTHORIZED = 401
-	NOT_FOUND    = 404
-)
--- configs/configs.go
+++
@@ -1,5 +0,0 @@
-// records configuration
-package configs
-
-// md文档路径
-const ROOT = "/home/wangduo/workspace/md_list"
